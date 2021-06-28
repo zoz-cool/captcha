164779d89bafea2a2b66c3f5d5b313e3ae0fb699
yooongchun <yooongchun@foxmail.com>
2021-06-28 15:07:15 +0800

    update some details

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 if __name__ == '__main__':
     res = requests.post('http://localhost:8899/captcha-label',
-                  files={'file': ('captcha_test.png', open('images-color/1622952867.692906.black.png', 'rb'))})
+                  files={'file': ('captcha_test.png', open('images/1622952867.692906.black.png', 'rb'))})
     print(res.json())
