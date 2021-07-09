8cf3452d619f9deb8913574f09c7c438dd54ad72
yczha <yooongchun@foxmail.com>
2021-07-09 17:02:04 +0800

    fix bug

--- model/config.py
+++ model/config.py
@@ -21,7 +21,7 @@ class Config(object):
     num_epochs = 100
     nw = max(os.cpu_count(), 8)
     learning_rate = 0.01
-    model_path = os.path.join(os.path.dirname(__file__), 'model/checkpoint/captcha.ctc-model-2.pth')  # 训练参数保存路径
+    model_path = os.path.join(os.path.dirname(__file__), 'model/checkpoint/captcha.ctc-model.pth')  # 训练参数保存路径
     dataset_path = 'dataset/captcha/'  # 数据集路径
     # 选择设备：GPU或CPU
     device_train = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
