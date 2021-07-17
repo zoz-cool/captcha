2a80088936381978d11b5319b659c79cf0ef487d
yooongchun <yooongchun@foxmail.com>
2021-07-17 12:26:39 +0800

    fix bug: test api

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 
+
 if __name__ == '__main__':
-    res = requests.post('http://localhost:8899/captcha-label',
+    res = requests.post('http://www.yooongchun.com:8081/captcha-label',
                   files={'file': ('captcha_test.png', open('images/1622952867.692906.black.png', 'rb'))})
     print(res.json())
