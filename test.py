03d2464f426179293332423180580a2fa118e938
yooongchun <yooongchun@foxmail.com>
2021-07-29 10:06:59 +0800

    change url in test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 
 if __name__ == '__main__':
-    res = requests.post('http://localhost:8081/captcha-label',
+    res = requests.post('http://captcha:5000/captcha-label',
                   files={'file': ('captcha_test.png', open('images/1622952867.692906.black.png', 'rb'))})
     if res.status_code == 200:
         print(res.json())
