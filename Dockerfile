c3c8ce515b89105cba11222227efa7bfd6e57e9a
ubuntu <ubuntu@localhost.localdomain>
2021-10-10 11:17:41 +0800

    flask替换为fastapi

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,10 @@ RUN groupadd -r yczha && useradd -r -g yczha yczha
 WORKDIR /home/yczha
 
 COPY requirements.txt requirements.txt
-COPY torch-1.9.0-cp38-cp38-manylinux1_x86_64.whl torch-1.9.0-cp38-cp38-manylinux1_x86_64.whl
 ARG PYPI=https://pypi.mirrors.ustc.edu.cn/simple/
 RUN pip install pip setuptools --upgrade -i ${PYPI}\
-    && pip install torch-1.9.0-cp38-cp38-manylinux1_x86_64.whl\
     && pip install -r requirements.txt -i ${PYPI}\
-    && rm requirements.txt torch-1.9.0-cp38-cp38-manylinux1_x86_64.whl && rm -rf /.pip
+    && rm requirements.txt && rm -rf /.pip
 
 COPY app.py ./
 COPY model model
@@ -18,4 +16,4 @@ ENV FLASK_APP app.py
 EXPOSE 5000
 
 USER yczha
-CMD exec gunicorn -w 4 -b :5000 --access-logfile - --error-logfile - app:app
+CMD exec uvicorn app:app --host 0.0.0.0 --port 5000
--- app.py
+++ app.py
@@ -1,21 +1,40 @@
-from flask import Flask, request, jsonify
-from model.predict import predict
+from typing import Optional
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 from PIL import Image
+from model.predict import predict
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route('/captcha-label', methods=['POST'])
-def return_captcha_label():
-    try:    
-        file_storage = request.files.get('file')
-        file_name = file_storage.filename
-        img = Image.open(file_storage.stream)
-        label =  predict(img)
-        return jsonify({'status_code': 200, 'filename': file_name, 'predict': label})
+class Response(BaseModel):
+    """响应对象"""
+    status_code: Optional[int] = 200
+    message: Optional[str] = '请求成功'
+    data: Optional[dict] = None
+
+
+@app.post('/captcha/label', response_model=Response)
+def get_captcha_label(file: UploadFile = File(...)):
+    """上传验证码图片返回识别标签"""
+    try:
+        img = Image.open(file.file)
+        label = predict(img)
+        return {'data': {'predict-label': label, 'filename': file.filename}}
     except Exception as e:
-        return jsonify({'status_code': 500, 'message': str(e)})
+        return {'status_code': 500, 'message': '请求失败：'+str(e)}
+
+
+def test():
+    """测试用例"""
+    import os
+    from fastapi.testclient import TestClient
+    client = TestClient(app)
+    file_path = 'test_case/DRCMCC_1503.png'
+    response = client.post('/captcha/label', files={
+        'file': (os.path.basename(file_path), open(file_path, "rb"), "image/png")})
+    print(response.json())
 
 
 if __name__ == '__main__':
-    app.run()
+    test()
