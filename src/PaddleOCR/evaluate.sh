8db6bb84abb15745e0c9ac154469b2ba579c4a39
yooongchun <yooongchun@foxmail.com>
2023-12-08 17:54:56 +0800

    refac[PaddleOCR]: 移除PaddleOCR

--- src/PaddleOCR/evaluate.sh
+++
@@ -1,1 +0,0 @@
-python3 tools/eval.py -c configs/rec/PP-OCRv3/ch_PP-OCRv3_rec_distillation.yml -o Global.checkpoints=./output/rec_ppocr_v3_distillation/best_accuracy
--- src/PaddleOCR/export_infer_model.sh
+++
@@ -1,1 +0,0 @@
-python3 tools/export_model.py -c configs/rec/PP-OCRv3/ch_PP-OCRv3_rec_distillation.yml -o Global.pretrained_model=./output/rec_ppocr_v3_distillation/best_accuracy  Global.save_inference_dir=./inference
