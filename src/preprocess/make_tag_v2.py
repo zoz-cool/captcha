493265aabf704d60826d809449ccdb9670c2df53
zhayongchun <zhayongchun@meituan.com>
2023-12-22 16:56:11 +0800

    perf[make tag]: add progress bar

--- src/preprocess/make_tag_v2.py
+++ src/preprocess/make_tag_v2.py
@@ -13,9 +13,10 @@ import json
 import pathlib
 
 import click
+import requests
 from glob import glob
 from PySide6.QtWidgets import QApplication, QListWidget, QHBoxLayout, QWidget, QPushButton, QLabel, QVBoxLayout, \
-    QDialog, QLineEdit, QMessageBox
+    QDialog, QLineEdit, QMessageBox, QProgressBar
 from PySide6.QtGui import QPixmap, QFont
 from PySide6.QtCore import QSize, Qt
 
@@ -23,15 +24,15 @@ root_dir = pathlib.Path(__file__).parent.parent.parent
 
 
 def get_predict_label(img_path: str):
-    return "nothing"
-    # res = requests.post("http://localhost:8000/captcha/predict", files={"files": open(img_path, "rb")})
-    # return res.json()["data"][0]["label"]
+    res = requests.post("http://localhost:8000/captcha/predict", files={"files": open(img_path, "rb")})
+    return res.json()["data"][0]["label"]
 
 
 class TagDialog(QDialog):
-    def __init__(self, img_path):
+    def __init__(self, img_path, enable_pred=False):
         super().__init__()
 
+        self.enable_pred = enable_pred
         # 布局
         self.setWindowTitle('Make Tag Window')
         self.setFixedSize(QSize(400, 300))
@@ -51,10 +52,10 @@ class TagDialog(QDialog):
 
         # 创建一个文本框用于输入标签
         channel = os.path.basename(img_path).split("-")[0]
-        self.text = get_predict_label(img_path)
+        self.text = get_predict_label(img_path) if self.enable_pred else "-"
         lbox = QHBoxLayout()
         lbox.addWidget(QLabel("Input"))
-        label = QLabel(channel)
+        label = QLabel(channel.upper())
         label.setStyleSheet(f"color: {channel}")
         label.setFont(QFont("Arial", 20))
         lbox.addWidget(label)
@@ -115,19 +116,24 @@ class TagDialog(QDialog):
 
 
 class TagWindow(QWidget):
-    def __init__(self, dataset_dir, target_dir, test_ratio=0.4):
+    def __init__(self, dataset_dir: pathlib.Path, target_dir: pathlib.Path, test_ratio: float = 0.4,
+                 enable_pred: bool = False):
         super().__init__()
 
-        # 布局
-        self.setWindowTitle('Make Tag Window')
-        self.setFixedSize(QSize(800, 600))
-
+        self.enable_pred = enable_pred
         self.dataset_dir = dataset_dir
         self.target_dir = target_dir
         self.test_ratio = test_ratio
 
         self.file_paths = glob(f"{self.dataset_dir}/*.png")
         self.filename_map = {os.path.basename(file_path): file_path for file_path in self.file_paths}
+
+        self.processed_num = len(glob(f"{self.target_dir}/*.png"))
+        self.total_num = len(self.file_paths) + self.processed_num
+
+        # ========UI布局============
+        self.setWindowTitle('Make Tag Window')
+        self.setFixedSize(QSize(800, 600))
 
         # 创建两个列表
         self.unprocessed_list = QListWidget()
@@ -152,7 +158,6 @@ class TagWindow(QWidget):
         v1.addWidget(self.unprocessed_label)
         v1.addWidget(self.unprocessed_list)
         layout.addLayout(v1)
-
         v2 = QVBoxLayout()
         v2.addWidget(self.process_button)
         v2.addWidget(quit_button)
@@ -163,7 +168,18 @@ class TagWindow(QWidget):
         v3.addWidget(self.processed_list)
         layout.addLayout(v3)
 
-        self.setLayout(layout)
+        # 创建一个进度条
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setMinimum(0)
+        self.progress_bar.setValue(self.processed_num)
+        self.progress_bar.setMaximum(self.total_num)
+        self.progress_bar.setFormat("Progress: %p%")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.progress_bar)
+        vbox.addLayout(layout)
+
+        self.setLayout(vbox)
 
     @staticmethod
     def tag_filename(filename, tag, idx, channel):
@@ -182,22 +198,23 @@ class TagWindow(QWidget):
             with open(json_file_path, "r") as f:
                 json_list = json.load(f)
         channel = filename.split("-")[0]
-        json_list.append({"filename": str(target_file_path), channel: tag})
+
+        json_list.append({"path": self.target_dir.name + "/" + filename, channel: tag})
         with open(json_file_path, "w") as f:
-            json.dump(json_list, f)
+            json.dump(json_list, f, ensure_ascii=False, indent=4)
         return tag_filename
 
     def process_file(self):
         # 检查未处理列表是否为空
-        index = len(glob(f"{self.target_dir}/*.png"))
-        for i in range(index, index + len(self.filename_map)):
+
+        for i in range(self.processed_num, self.total_num):
             # 从未处理列表中取出一个文件
             file_item = self.unprocessed_list.currentItem() or self.unprocessed_list.item(0)
             filename = file_item.text()
             self.unprocessed_list.setCurrentItem(file_item)
 
             # 处理文件
-            dialog = TagDialog(self.filename_map[filename])
+            dialog = TagDialog(self.filename_map[filename], self.enable_pred)
             result = dialog.exec()
             # 删除指定行的项
             row = self.unprocessed_list.row(file_item)
@@ -211,10 +228,12 @@ class TagWindow(QWidget):
                 tag_filename = self.save_image(filename, tag, i)
                 self.processed_list.addItem(tag_filename)
                 self.processed_label.setText(f"已处理:{self.processed_list.count()}")
+                self.progress_bar.setValue(i)
             elif result == QDialog.Rejected:
                 break
             elif result == 11:
                 self.unprocessed_label.setText(f"未处理:{self.unprocessed_list.count()}")
+                self.progress_bar.setValue(i)
                 continue
             else:
                 raise ValueError("unknown result")
@@ -224,13 +243,14 @@ class TagWindow(QWidget):
 @click.option("--dataset-dir", type=str, default="origin")
 @click.option("--output-dir", type=str, default="labeled")
 @click.option("--test-ratio", type=float, default=0.4)
-def main(dataset_dir, output_dir, test_ratio):
+@click.option("--enable-pred", type=bool, default=False)
+def main(dataset_dir, output_dir, test_ratio, enable_pred):
     dataset_dir = root_dir / "dataset" / dataset_dir
     save_dir = root_dir / "dataset" / output_dir / "images"
     os.makedirs(save_dir, exist_ok=True)
 
     app = QApplication()
-    win = TagWindow(dataset_dir, save_dir, test_ratio)
+    win = TagWindow(dataset_dir, save_dir, test_ratio, enable_pred)
     win.show()
     app.exec()
 
